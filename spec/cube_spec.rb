793e74e92beb89e239915c015262f83e1fa8644e
drKreso <kresimir.bojcic@gmail.com>
2012-02-18 14:53:36 +0100

    returns array and not | delimited string

--- spec/cube_spec.rb
+++ spec/cube_spec.rb
@@ -20,8 +20,8 @@ describe XMLA::Cube do
    VCR.use_cassette('kvatovi_u_koloni') do
       result = XMLA::Cube.execute("select [Lokacija].[Kvart].children  on COLUMNS, [Measures].[Broj] on ROWS from [GOSJAR]") 
       result.size.should == 2
-      result[0].split('|').size.should == 18
-      result[1].should == "Broj|1422|2259|2148|2733|2004|2607|2829|1611|2581|1945|3602|1356|1696|2327|1228|3186|"
+      result[0].size.should == 18
+      result[1].join('|').should == "Broj|1422|2259|2148|2733|2004|2607|2829|1611|2581|1945|3602|1356|1696|2327|1228|3186|"
    end
   end
 
@@ -29,9 +29,9 @@ describe XMLA::Cube do
    VCR.use_cassette('kvartovi_u_recima') do
       result =  XMLA::Cube.execute("select [Measures].[Broj]  on COLUMNS, non empty topcount( [Lokacija].[Kvart].children, 100,  [Measures].[Broj]) on ROWS from [GOSJAR]")
       result.size.should == 17
-      result[0].should == '|Broj'
-      result[6].should == 'TRESNJEVKA - JUG|2607'
-      result[16].should == 'PODSLJEME|1228'
+      result[0].join('|').should == '|Broj'
+      result[6].join('|').should == 'TRESNJEVKA - JUG|2607'
+      result[16].join('|').should == 'PODSLJEME|1228'
     end
   end
 
@@ -39,9 +39,9 @@ describe XMLA::Cube do
    VCR.use_cassette('razlog_prijave_i_kvart') do
      result = XMLA::Cube.execute("select [Measures].[Broj]  on COLUMNS, non empty topcount ( [Razlog prijave].[Razlog].children * [Lokacija].[Kvart].children , 20,  [Measures].[Broj] ) on ROWS from [GOSJAR]")
       result.size.should == 21
-      result[0].should == "||Broj"
-      result[2].should == "|GORNJA DUBRAVA|1383"
-      result[20].should == "Redovna izmjena|GORNJA DUBRAVA|575"
+      result[0].join('|').should == "||Broj"
+      result[2].join('|').should == "|GORNJA DUBRAVA|1383"
+      result[20].join('|').should == "Redovna izmjena|GORNJA DUBRAVA|575"
     end
   end
 
@@ -64,8 +64,8 @@ describe XMLA::Cube do
    VCR.use_cassette('mondrian_broj_intervencija') do
     result = XMLA::Cube.execute("SELECT NON EMPTY {Hierarchize({[Measures].[Broj intervencija]})} ON COLUMNS, NON EMPTY {Hierarchize({[Gradska cetvrt].[Gradska cetvrt].Members})} ON ROWS FROM [Kvarovi]")
     result.size.should == 17
-    result[0].should == "|Broj intervencija"
-    result[2].should == "GORNJI GRAD – MEDVEŠČAK|2259"
+    result[0].join('|').should == "|Broj intervencija"
+    result[2].join('|').should == "GORNJI GRAD – MEDVEŠČAK|2259"
    end
   end
 
@@ -79,8 +79,8 @@ describe XMLA::Cube do
        FROM [Kvarovi]
     MDX
     result.size.should == 2
-    result[0].should == "|Broj intervencija"
-    result[1].should == "Ne radi svjetiljka|14442"
+    result[0].join('|').should == "|Broj intervencija"
+    result[1].join('|').should == "Ne radi svjetiljka|14442"
    end
   end
 
